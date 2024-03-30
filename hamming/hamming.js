0967938f9d5d161770943241d8ba7b65a256b248
fabry-js <vivaldifabrizio10@gmail.com>
2024-03-30 21:48:10 +0100

    feat(hamming): implement hamming exercise

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -3,6 +3,19 @@
 // convenience to get you started writing code faster.
 //
 
-export const compute = () => {
-  throw new Error("Remove this statement and implement this function");
+export const compute = (sequence1, sequence2) => {
+  if (typeof sequence1 !== "string" || typeof sequence2 !== "string")
+    throw new TypeError("Provided sequences are not of type string");
+  if (sequence1.length !== sequence2.length)
+    throw new Error("strands must be of equal length");
+
+  let errorCount = 0;
+
+  for (let i = 0; i <= sequence1.length; i++) {
+    if (sequence1[i] !== sequence2[i]) {
+      errorCount++;
+    }
+  }
+
+  return errorCount;
 };
