eaea4e405d4a99bd5f5a848e0127b42022c1679b
fabry-js <vivaldifabrizio10@gmail.com>
2024-03-30 23:13:16 +0100

    feat: implement anagrams

--- anagram/anagram.js
+++ anagram/anagram.js
@@ -3,6 +3,25 @@
 // convenience to get you started writing code faster.
 //
 
-export const findAnagrams = () => {
-  throw new Error("Remove this statement and implement this function");
+const splitAndSort = (input) => input.toLowerCase().split("").sort().join("");
+
+export const findAnagrams = (inputString, _searchArray) => {
+  // Type check
+  if (typeof inputString !== "string")
+    throw new Error("Input is not a valid string");
+  const searchArray = [..._searchArray];
+
+  const searchWord = splitAndSort(inputString);
+  const results = [];
+
+  searchArray.forEach((originalWord) => {
+    const word = splitAndSort(originalWord);
+    if (
+      searchWord === word &&
+      originalWord.toLowerCase() !== inputString.toLowerCase()
+    )
+      results.push(originalWord);
+  });
+
+  return results;
 };
