2254d9756a3d49b8bdd63f41e82d0c64e93dfcc4
fabry-js <vivaldifabrizio10@gmail.com>
2024-03-31 23:02:35 +0200

    feat: implement nucleotide-count

--- nucleotide-count/nucleotide-count.js
+++ nucleotide-count/nucleotide-count.js
@@ -4,5 +4,18 @@
 //
 
 export function countNucleotides(strand) {
-  throw new Error("Remove this statement and implement this function");
+  if (typeof strand !== "string")
+    throw new TypeError("Provided strand is not a string");
+
+  const regex = /^[ACGT]*$/;
+  if (!regex.test(strand)) throw new Error("Invalid nucleotide in strand");
+
+  let nucleotideCount = { A: 0, C: 0, G: 0, T: 0 };
+  const splittedSequence = strand.split("");
+
+  splittedSequence.forEach((nucleotide) => {
+    nucleotideCount[nucleotide]++;
+  });
+
+  return Object.values(nucleotideCount).join(" ");
 }
