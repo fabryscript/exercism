f31af47b4b9e1d20d7c7d09cac0ea6ebf4c5388f
fabry-js <vivaldifabrizio10@gmail.com>
2024-04-01 23:50:06 +0200

    feat: implement scrabble-score

--- scrabble-score/scrabble-score.js
+++ scrabble-score/scrabble-score.js
@@ -3,7 +3,38 @@
 // convenience to get you started writing code faster.
 //
 
-export const score = () => {
-  throw new Error("Remove this statement and implement this function");
+const getScore = (char) => {
+  const tests = [
+    { exp: /[AEIOULNRST]/, score: 1 },
+    { exp: /[DG]/, score: 2 },
+    { exp: /[BCMP]/, score: 3 },
+    { exp: /[FHVMY]/, score: 4 },
+    { exp: /[K]/, score: 5 },
+    { exp: /[JX]/, score: 8 },
+    { exp: /[QZ]/, score: 10 },
+  ];
+
+  let charScore = 0;
+
+  tests.forEach(({ exp, score }) => {
+    if (exp.test(char.toUpperCase())) charScore += score;
+  });
+
+  return charScore;
 };
-a;
+
+export const score = (word) => {
+  if (typeof word !== "string")
+    throw new TypeError("Input is not a valid string");
+
+  if (word === "") return 0;
+
+  let score = 0;
+
+  word.split("").forEach((currChar) => {
+    const charScore = getScore(currChar);
+    score += charScore;
+  });
+
+  return score;
+};
