1886b94befa328e36767a55ee53068d8c323b6cf
fabryscript <vivaldifabrizio10@gmail.com>
2024-04-07 21:01:37 +0200

    feat: WIP implement forth

--- forth/forth.ts
+++ forth/forth.ts
@@ -3,35 +3,68 @@
 // convenience to get you started writing code faster.
 //
 
+class Matrix<T> {
+  private _matrix: T[][] = [];
+
+  constructor(initial: T[][]) {
+    this._matrix = initial;
+  }
+
+  pushRow(row: T[]) {
+    this._matrix.push(row);
+  }
+
+  getIndexByItem(item: T, col: number = 0) {
+    let columnIndex = -1;
+
+    for (let i = 0; i < this._matrix.length; i++) {
+      if (this._matrix[i][col] === item) {
+        columnIndex = i;
+        break;
+      }
+    }
+
+    return columnIndex;
+  }
+
+  getValueFromKey(key: T) {
+    return this._matrix[this.getIndexByItem(key, 0)][1];
+  }
+
+  get() {
+    return this._matrix;
+  }
+}
+
 export class Forth {
-  #stack = [];
+  private _stack: number[] = [];
 
   ops = {
-    "+": (x, y) => x + y,
-    "-": (x, y) => x - y,
-    "/": (x, y) => {
+    "+": (x: number, y: number) => x + y,
+    "-": (x: number, y: number) => x - y,
+    "/": (x: number, y: number) => {
       if (y === 0) {
         throw new Error("Division by zero");
       }
 
       return x / y;
     },
-    "*": (x, y) => x * y,
-    dup: (s) => {
+    "*": (x: number, y: number) => x * y,
+    dup: (s: typeof this._stack) => {
       if (s.length === 0) {
         throw new Error("Stack empty");
       }
       s.push(this.at(s, -1));
       return s;
     },
-    drop: (s) => {
+    drop: (s: typeof this._stack) => {
       if (s.length === 0) {
         throw new Error("Stack empty");
       }
       s.pop();
       return s;
     },
-    swap: (s) => {
+    swap: (s: typeof this._stack) => {
       if (s.length < 2) {
         throw new Error("Stack empty");
       }
@@ -42,7 +75,7 @@ export class Forth {
 
       return s;
     },
-    over: (s) => {
+    over: (s: typeof this._stack) => {
       if (s.length < 2) {
         throw new Error("Stack empty");
       }
@@ -54,20 +87,22 @@ export class Forth {
     },
   };
 
-  aliases = {
-    "+": "+",
-    "-": "-",
-    "/": "/",
-    "*": "*",
-    dup: "dup",
-    drop: "drop",
-    swap: "swap",
-    over: "over",
-  };
+  aliases = new Matrix<string>([
+    ["+", "+"],
+    ["-", "-"],
+    ["/", "/"],
+    ["*", "*"],
+    ["dup", "dup"],
+    ["drop", "drop"],
+    ["swap", "swap"],
+    ["over", "over"],
+  ]);
 
   constructor() {
-    this.#stack = [];
+    this._stack = [];
   }
+
+  getIndexByKey() {}
 
   /**
    * Gets an item of an array based by an index which could also be negative
@@ -75,13 +110,13 @@ export class Forth {
    * @param i the index, which can also be negative
    * @returns The item of the array or string
    */
-  at(input, i) {
+  at(input: string | any[], i: number) {
     const idx = i < 0 ? (i + input.length) % input.length : i;
     return input[idx];
   }
 
-  evaluate(_input) {
-    const input = _input.trim().toLowerCase();
+  evaluate(_input: string) {
+    const input = _input.toLowerCase().trim();
     /**
      * This means that the word is a new alias definition
      */
@@ -92,19 +127,29 @@ export class Forth {
       const name = split[0];
       if (Number.isInteger(+name)) throw new Error("Invalid definition");
       split.shift();
-      const command = split.join(" ");
 
-      this.aliases[name] = command;
+      const replacedCommand = split.map((keyword) => {
+        const index = this.aliases.getIndexByItem(keyword);
+        // We are searching if there is a column with the keyword value
+        if (this.aliases.get()[index][0]) {
+          return this.aliases.getValueFromKey(keyword);
+        }
+        return keyword;
+      });
+
+      const command = replacedCommand.join(" ");
+
+      this.aliases.pushRow([name, command]);
       return;
     }
 
-    const aliases = Object.keys(this.aliases);
     const replacedInput = input
       .split(" ")
       .map((keyword) => {
         if (Number.isInteger(+keyword)) return keyword;
-        if (aliases.includes(keyword)) {
-          return this.aliases[keyword];
+        // continue from here
+        if (this.aliases.get()) {
+          return this.aliases[1][keyword] as string;
         }
 
         throw new Error("Unknown command");
@@ -118,12 +163,13 @@ export class Forth {
        * it into the stack
        */
       if (!Number.isNaN(+_word)) {
-        return this.#stack.push(+_word);
+        return this._stack.push(+_word);
       }
       /**
        * This means that the word is an operation
        */
-      const op = this.aliases[_word];
+      console.log(_word);
+      const op = _word as keyof typeof this.ops;
       switch (op) {
         case "+":
         case "-":
@@ -133,22 +179,22 @@ export class Forth {
            * An operation should ideally be performed on the last two
            * items of the stack
            */
-          if (this.#stack.length < 2) {
+          if (this._stack.length < 2) {
             throw new Error("Stack empty");
           }
 
-          const lastItem = this.at(this.#stack, -1);
-          const secondLastItem = this.at(this.#stack, -2);
+          const lastItem = this.at(this._stack, -1);
+          const secondLastItem = this.at(this._stack, -2);
 
           const result = this.ops[op](secondLastItem, lastItem);
 
-          this.#stack.splice(-2, 2, Math.floor(result));
+          this._stack.splice(-2, 2, Math.floor(result));
           break;
         case "dup":
         case "drop":
         case "swap":
         case "over":
-          this.#stack = this.ops[op](this.#stack);
+          this._stack = this.ops[op](this._stack);
         default:
           break;
       }
@@ -157,6 +203,12 @@ export class Forth {
   }
 
   get stack() {
-    return this.#stack;
+    return this._stack;
   }
 }
+
+const inst = new Forth();
+inst.evaluate(": foo * ;");
+inst.evaluate("10 2 foo");
+
+console.log(inst.stack);
