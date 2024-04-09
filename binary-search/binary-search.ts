f2696930b57db1ed830e69e5e8645556022f20e2
fabryscript <vivaldifabrizio10@gmail.com>
2024-04-10 00:05:43 +0200

    feat: implement binary-search

--- binary-search/binary-search.ts
+++ binary-search/binary-search.ts
@@ -3,6 +3,26 @@
 // convenience to get you started writing code faster.
 //
 
-export const find = () => {
-  throw new Error("Remove this statement and implement this function");
+export const find = (arr: number[], searchValue: number) => {
+  if (arr.length === 0) throw new Error("Value not in array");
+
+  const sorted = arr.sort((a, b) => (a > b ? 1 : -1));
+  let first = 0;
+
+  let last = sorted.length - 1;
+
+  if (searchValue > sorted[last]) throw new Error("Value not in array");
+
+  while (first <= last) {
+    let index = Math.floor((first + last) / 2);
+
+    if (sorted[index] === searchValue) return index;
+    else if (sorted[index] > searchValue) {
+      last = index - 1;
+    } else {
+      first = index + 1;
+    }
+  }
+
+  throw new Error("Value not in array");
 };
