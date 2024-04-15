d2f7877a052f2b5c6409fe06747a16f446374f7a
fabryscript <vivaldifabrizio10@gmail.com>
2024-04-15 21:52:12 +0200

    feat: implement resistor-color-trio

--- resistor-color-trio/resistor-color-trio.js
+++ resistor-color-trio/resistor-color-trio.js
@@ -3,12 +3,48 @@
 // convenience to get you started writing code faster.
 //
 
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
+
 export class ResistorColorTrio {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  values = [];
+
+  constructor(values) {
+    this.values = values;
   }
 
-  label() {
-    throw new Error('Remove this statement and implement this function');
+  getValue(value) {
+    const index = COLORS.indexOf(value);
+
+    if (index !== -1) return index;
+
+    throw new Error("invalid color");
+  }
+
+  getScaledTotal(value) {
+    if (value >= 1000) {
+      return `${value / 1000} kilo`;
+    }
+    return `${value} `;
+  }
+
+  get label() {
+    const firstValue = this.getValue(this.values[0]);
+    const secondValue = this.getValue(this.values[1]);
+    const exp = this.getValue(this.values[2]);
+
+    const total = +`${firstValue}${secondValue}` * +"1".padEnd(exp + 1, "0");
+
+    return `Resistor value: ${this.getScaledTotal(total)}ohms`;
   }
 }
--- resistor-color-trio/resistor-color-trio.ts
+++ resistor-color-trio/resistor-color-trio.ts
@@ -3,12 +3,48 @@
 // convenience to get you started writing code faster.
 //
 
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
+
 export class ResistorColorTrio {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  values: string[] = [];
+
+  constructor(values: string[]) {
+    this.values = values;
   }
 
-  label() {
-    throw new Error("Remove this statement and implement this function");
+  private getValue(value: string) {
+    const index = COLORS.indexOf(value);
+
+    if (index !== -1) return index;
+
+    throw new Error("invalid color");
+  }
+
+  private getScaledTotal(value: number) {
+    if (value >= 1000) {
+      return `${value / 1000} kilo`;
+    }
+    return `${value} `;
+  }
+
+  get label() {
+    const firstValue = this.getValue(this.values[0]);
+    const secondValue = this.getValue(this.values[1]);
+    const exp = this.getValue(this.values[2]);
+
+    const total = +`${firstValue}${secondValue}` * +"1".padEnd(exp + 1, "0");
+
+    return `Resistor value: ${this.getScaledTotal(total)}ohms`;
   }
 }
