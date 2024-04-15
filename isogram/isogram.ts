397680beb137316f32e7ddd650c7338788d175c9
fabryscript <vivaldifabrizio10@gmail.com>
2024-04-15 11:40:17 +0200

    feat: finish implementing isogram

--- isogram/isogram.ts
+++ isogram/isogram.ts
@@ -5,11 +5,15 @@
 
 // I hate not having the right ts libs...
 
-function replaceAll(input, search, replacement) {
+function replaceAll(
+  input: string,
+  search: string | RegExp,
+  replacement: string
+): string {
   return input.split(search).join(replacement);
 }
 
-export const isIsogram = (word) => {
+export const isIsogram = (word: string) => {
   if (word === "") return true;
 
   const split = replaceAll(word.toLowerCase(), /[\s-]/g, "").split("");
@@ -17,3 +21,5 @@ export const isIsogram = (word) => {
 
   return split.length === set.size;
 };
+
+console.log(isIsogram("angola"));
