d2f15723f1c0b8df18011cc4b8453cdba4e86aa7
fabryscript <vivaldifabrizio10@gmail.com>
2024-05-05 12:12:00 +0200

    feat: implement perfect-numbers

--- perfect-numbers/perfect-numbers.js
+++ perfect-numbers/perfect-numbers.js
@@ -3,6 +3,32 @@
 // convenience to get you started writing code faster.
 //
 
-export const classify = () => {
-  throw new Error('Remove this statement and implement this function');
+const getFactors = (num) => {
+  const factors = [];
+
+  for (let i = 1; i < num; i++) {
+    if (num % i === 0) {
+      factors.push(i);
+    }
+  }
+
+  return factors;
+};
+
+export const classify = (num) => {
+  if (num <= 0) {
+    throw new Error("Classification is only possible for natural numbers.");
+  }
+
+  const factors = getFactors(num);
+  const aliquotSum = factors.reduce(
+    (prevTotal, value) => (prevTotal += value),
+    0
+  );
+
+  if (num === aliquotSum) return "perfect";
+  if (num < aliquotSum) return "abundant";
+  if (num > aliquotSum) return "deficient";
+
+  throw new Error("Unexpected Error occurred");
 };
