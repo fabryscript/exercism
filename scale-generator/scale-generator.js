b59184bc24003f2e255e0068ec869786f6a16f3d
fabryscript <vivaldifabrizio10@gmail.com>
2024-05-10 00:28:26 +0200

    feat: implement scale-generator

--- scale-generator/scale-generator.js
+++ scale-generator/scale-generator.js
@@ -3,16 +3,99 @@
 // convenience to get you started writing code faster.
 //
 
+const FLATS_CHROMATIC = [
+  "C",
+  "Db",
+  "D",
+  "Eb",
+  "E",
+  "F",
+  "Gb",
+  "G",
+  "Ab",
+  "A",
+  "Bb",
+  "B",
+];
+
+const SHARPS_CHROMATIC = [
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#",
+  "A",
+  "A#",
+  "B",
+];
+
+const NATURAL_ROOTS = ["C", "a"];
+
+const FLATS_ROOTS = [
+  "F",
+  "Bb",
+  "Eb",
+  "Ab",
+  "Db",
+  "Gb",
+  "d",
+  "g",
+  "c",
+  "f",
+  "bb",
+  "eb",
+];
+
+const STEPS = {
+  m: 1,
+  M: 2,
+  A: 3,
+};
+
 export class Scale {
+  #rootNote = "";
+  #selectedArray = [];
+
   constructor(tonic) {
-    throw new Error("Remove this statement and implement this function");
+    this.#rootNote = tonic;
+    this.#selectedArray =
+      FLATS_ROOTS.includes(this.#rootNote) &&
+      !NATURAL_ROOTS.includes(this.#rootNote)
+        ? FLATS_CHROMATIC
+        : SHARPS_CHROMATIC;
   }
 
   chromatic() {
-    throw new Error("Remove this statement and implement this function");
+    const rootNote =
+      this.#rootNote.charAt(0).toUpperCase() + this.#rootNote.slice(1);
+
+    return this.from(rootNote);
   }
 
   interval(intervals) {
-    throw new Error("Remove this statement and implement this function");
+    const convertedInterval = this.convertIntervals(intervals);
+    const chromatic = this.chromatic();
+
+    const convertedSteps = convertedInterval.map((_, i) =>
+      convertedInterval.slice(0, i).reduce((p, c) => (p + c) % 12, 0)
+    );
+
+    return convertedSteps.map((chromaticIndex) => chromatic[chromaticIndex]);
+  }
+
+  convertIntervals(intervals) {
+    return [...intervals.split("").map((interval) => STEPS[interval]), 1];
+  }
+
+  from(note) {
+    const index = this.#selectedArray.indexOf(note);
+
+    return this.#selectedArray
+      .slice(index)
+      .concat(this.#selectedArray.slice(0, index));
   }
 }
