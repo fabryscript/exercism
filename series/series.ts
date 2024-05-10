7e4b2a83b06771122562ffde4b2a0091606f64e2
fabryscript <vivaldifabrizio10@gmail.com>
2024-05-10 22:17:27 +0200

    feat: implement series

--- series/series.ts
+++ series/series.ts
@@ -4,11 +4,30 @@
 //
 
 export class Series {
+  #series: string;
+
   constructor(series: string) {
-    throw new Error("Remove this statement and implement this function");
+    this.#series = series;
   }
 
   slices(sliceLength: number) {
-    throw new Error("Remove this statement and implement this function");
+    if (this.#series.length === 0) throw new Error("series cannot be empty");
+    if (sliceLength < 0) throw new Error("slice length cannot be negative");
+    if (sliceLength === 0) throw new Error("slice length cannot be zero");
+    if (sliceLength > this.#series.length)
+      throw new Error("slice length cannot be greater than series length");
+
+    const results: number[][] = [];
+
+    for (let i = 0; i < this.#series.length - sliceLength + 1; i++) {
+      results.push(
+        this.#series
+          .slice(i, i + sliceLength)
+          .split("")
+          .map((c) => +c)
+      );
+    }
+
+    return results;
   }
 }
