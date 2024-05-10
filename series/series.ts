32846febd1807772e89b5daccf091908d13765fd
fabryscript <vivaldifabrizio10@gmail.com>
2024-05-10 21:51:43 +0200

    feat: init series

---
+++ series/series.ts
@@ -0,0 +1,14 @@
+//
+// This is only a SKELETON file for the 'Series' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Series {
+  constructor(series: string) {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  slices(sliceLength: number) {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
