f0c43e2b3b8fc5d2f8d4cb8e356cab44e818b11e
fabryscript <vivaldifabrizio10@gmail.com>
2024-05-10 23:01:32 +0200

    feat: implement largest-series-product

--- largest-series-product/largest-series-product.ts
+++ largest-series-product/largest-series-product.ts
@@ -3,6 +3,31 @@
 // convenience to get you started writing code faster.
 //
 
-export const largestProduct = () => {
-  throw new Error("Remove this statement and implement this function");
+const slices = (series: string, sliceLength: number) => {
+  const results: number[][] = [];
+
+  for (let i = 0; i < series.length - sliceLength + 1; i++) {
+    results.push(
+      series
+        .slice(i, i + sliceLength)
+        .split("")
+        .map(Number)
+    );
+  }
+
+  return results;
+};
+
+export const largestProduct = (series: string, length: number) => {
+  if (length > series.length || series.trim() === "")
+    throw new Error("Span must be smaller than string length");
+  if (/[a-zA-Z]+/g.test(series))
+    throw new Error("Digits input must only contain digits");
+  if (length < 0) throw new Error("Span must be greater than zero");
+
+  const products = slices(series, length).map((serie) =>
+    serie.reduce((total, curr) => total * curr)
+  );
+
+  return Math.max(...products);
 };
